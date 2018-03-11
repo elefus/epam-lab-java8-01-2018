20d40726efacabb51b8d7d9558aeeb0e15c6673c
timaliberdov <timur.aliberdov@yandex.ru>
2018-03-11 17:11:11 +0300

    spliterators.exercise1

--- src/test/java/spliterators/exercise/example1/UnfairRectangleSpliterator.java
+++ src/test/java/spliterators/exercise/example1/UnfairRectangleSpliterator.java
@@ -1,9 +1,14 @@
 package spliterators.exercise.example1;
 
+import java.util.Arrays;
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSpliterator {
+
+    private final int[][] data;
+    private int start, end;
 
     /**
      * 0 1 2 3 4
@@ -15,32 +20,41 @@ public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSplitera
      * 2 3 / 4 5 6
      * 2 4 5 6 7
      */
-    public UnfairRectangleSpliterator(int[][] data) {
-        this();
+    private UnfairRectangleSpliterator(int[][] data, int start, int end) {
+        super(end - start,
+            Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.NONNULL);
+        this.data = data;
+        this.start = start;
+        this.end = end;
     }
 
-    private UnfairRectangleSpliterator() {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    UnfairRectangleSpliterator(int[][] data) {
+        this(data, 0, data.length);
     }
 
     @Override
     public OfInt trySplit() {
-        throw new UnsupportedOperationException();
+        final int s = start;
+        start += (end - start) / 2;
+        return new UnfairRectangleSpliterator(data, s, start);
     }
 
     @Override
     public long estimateSize() {
-        throw new UnsupportedOperationException();
+        return end - start;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        throw new UnsupportedOperationException();
+        if(start == end) return false;
+        Arrays.stream(data[start++]).forEach(action);
+        return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        throw new UnsupportedOperationException();
+        while (start != end) {
+            Arrays.stream(data[start++]).forEach(action);
+        }
     }
 }
