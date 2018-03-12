ba119527e3e797654d74773c86cd7b67d6159e75
denklewer <denklewer@gmail.com>
2018-03-12 14:15:31 +0300

    exercise 2 completed

--- src/test/java/streams/part2/example/data/PersonCompanyDuration.java
+++ src/test/java/streams/part2/example/data/PersonCompanyDuration.java
@@ -1,4 +1,27 @@
 package streams.part2.example.data;
 
+import lambda.data.Person;
+
 public class PersonCompanyDuration {
+    private final Person person;
+    private final String company;
+    private final int duration;
+
+    public PersonCompanyDuration(Person person, String company, int duration) {
+        this.person = person;
+        this.company = company;
+        this.duration = duration;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
--- src/test/java/streams/part2/exercise/Exercise2.java
+++ src/test/java/streams/part2/exercise/Exercise2.java
@@ -1,12 +1,19 @@
 package streams.part2.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
+import streams.part2.example.data.PersonCompanyDuration;
+import streams.part2.example.data.PersonCompanyPair;
+
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("ConstantConditions")
@@ -68,7 +75,10 @@ public class Exercise2 {
     public void employersStuffList() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream()
+                                                   .parallel()
+                                                   .flatMap(Exercise2::getPersonCompanyPairStream)
+                                                   .collect(HashMap::new, Exercise2::mutableAddToMap, Exercise2::mutableMergeMaps);
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
@@ -89,6 +99,30 @@ public class Exercise2 {
         )));
         assertEquals(expected, result);
     }
+
+    private static Stream<PersonCompanyPair> getPersonCompanyPairStream(Employee employee) {
+        return employee.getJobHistory()
+                       .stream()
+                       .map(JobHistoryEntry::getEmployer)
+                       .map(company -> new PersonCompanyPair(employee.getPerson(), company));
+    }
+
+    private static void mutableAddToMap(Map<String, Set<Person>> map, PersonCompanyPair pair) {
+        map.compute(pair.getCompany(), (position, persons) -> {
+            persons = persons == null ? new HashSet<>() : persons;
+            persons.add(pair.getPerson());
+            return persons;
+        });
+    }
+
+    private static void mutableMergeMaps(HashMap<String, Set<Person>> left, HashMap<String, Set<Person>> right) {
+        right.forEach((position, persons) -> left.merge(position, persons,
+                (leftPersons, rightPersons) -> {
+                    leftPersons.addAll(rightPersons);
+                    return leftPersons;
+                }));
+    }
+
 
     /**
      * Преобразовать список сотрудников в отображение [компания -> множество людей, начавших свою карьеру в этой компании].
@@ -142,7 +176,11 @@ public class Exercise2 {
     public void indexByFirstEmployer() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream()
+                                                   .map(employee -> new PersonCompanyPair(employee.getPerson(),
+                                                                                          employee.getJobHistory().iterator().next().getEmployer()))
+                                                   .collect(groupingBy(PersonCompanyPair::getCompany,
+                                                                       mapping(PersonCompanyPair::getPerson, toSet())));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
@@ -166,11 +204,23 @@ public class Exercise2 {
     public void greatestExperiencePerEmployer() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Person> collect = null;
+        Map<String, Person> collect = employees.stream()
+                                               .parallel()
+                                               .flatMap(e -> e.getJobHistory()
+                                                              .stream()
+                                                              .collect(groupingBy(JobHistoryEntry::getEmployer,
+                                                                                  summingInt(JobHistoryEntry::getDuration)))
+                                                              .entrySet()
+                                                              .stream()
+                                                              .map(entry -> new PersonCompanyDuration(e.getPerson(), entry.getKey(), entry.getValue())))
+                                               .collect(groupingBy(PersonCompanyDuration::getCompany,
+                                                                   collectingAndThen(maxBy(Comparator.comparingInt(PersonCompanyDuration::getDuration)),
+                                                                                           entry -> entry.orElseThrow(IllegalStateException::new)
+                                                                                                         .getPerson())));
 
         Map<String, Person> expected = new HashMap<>();
         expected.put("EPAM", employees.get(4).getPerson());
-        expected.put("google", employees.get(1).getPerson());
+        expected.put("google", employees.get(0).getPerson()); // there are two guys ever worked in google with same experience
         expected.put("yandex", employees.get(2).getPerson());
         expected.put("mail.ru", employees.get(2).getPerson());
         expected.put("T-Systems", employees.get(5).getPerson());
