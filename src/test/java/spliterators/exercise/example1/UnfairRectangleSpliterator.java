1de21c711d0f6719caff542fde9dc2e50e384bfc
SergeyAlbrant <albrants95@mail.ru>
2018-03-12 14:59:14 +0300

    done spliterators.exercise1 (Unfair solution)

--- src/test/java/spliterators/exercise/example1/UnfairRectangleSpliterator.java
+++ src/test/java/spliterators/exercise/example1/UnfairRectangleSpliterator.java
@@ -1,10 +1,16 @@
 package spliterators.exercise.example1;
 
+import java.util.Arrays;
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
+    private int startInclusive;
+    private int endExclusive;
+
+    private int[][] data;
     /**
      * 0 1 2 3 4
      * ---------
@@ -16,31 +22,40 @@ public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSplitera
      * 2 4 5 6 7
      */
     public UnfairRectangleSpliterator(int[][] data) {
-        this();
+        this(data, 0, data.length);
     }
 
-    private UnfairRectangleSpliterator() {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    private UnfairRectangleSpliterator(int[][] data, int startInclusive, int endExclusive) {
+        super(data.length, Spliterator.IMMUTABLE | Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.SIZED);
+        this.data = data;
+        this.startInclusive = startInclusive;
+        this.endExclusive = endExclusive;
     }
 
     @Override
     public OfInt trySplit() {
-        throw new UnsupportedOperationException();
+        int mid = startInclusive + (int)(estimateSize() / 2);
+        return new UnfairRectangleSpliterator(data, startInclusive, startInclusive = mid);
     }
 
     @Override
     public long estimateSize() {
-        throw new UnsupportedOperationException();
+        return endExclusive - startInclusive;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        throw new UnsupportedOperationException();
+        if (startInclusive == endExclusive) {
+            return false;
+        }
+        Arrays.stream(data[startInclusive++]).forEach(action);
+        return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        throw new UnsupportedOperationException();
+        while (startInclusive != endExclusive) {
+            Arrays.stream(data[startInclusive++]).forEach(action);
+        }
     }
 }
